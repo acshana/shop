2c65b89cb269f6e814cb28f7f438a299932409b9
acshana <ac.one.yao@gmail.com>
2018-01-23 20:34:14 +0800

    设置media路径

--- Shop/urls.py
+++ Shop/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from Shop.settings import MEDIA_ROOT
+from django.views.static import serve
+
+
 # from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT})
 ]
-    
