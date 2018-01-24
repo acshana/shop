df1e85c39ef729122442579e4e31c46714c53d2c
acshana <ac.one.yao@gmail.com>
2018-01-24 17:19:35 +0800

    获取全部外键字段

--- apps/goods/ModelSerializer.py
+++ apps/goods/ModelSerializer.py
@@ -2,10 +2,15 @@
 # -*- coding:utf-8 -*-
 # @Time:2018/1/24 15:12
 from rest_framework import serializers
-from .models import Goods
+from .models import Goods,GoodsCategory
 
+class GoodsCategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields="__all__"
 
 class GoodsModelSerializer(serializers.ModelSerializer):
+    category=GoodsCategoryModelSerializer()
     class Meta:
         model = Goods
         fields="__all__"
