81a0285c253d952cd0f1397120ca82b9dc78cd79
acshana <ac.one.yao@gmail.com>
2018-01-24 16:36:47 +0800

    serializer实现post提交数据库

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -2,9 +2,15 @@
 # -*- coding:utf-8 -*-
 # @Time:2018/1/24 15:12
 from rest_framework import serializers
-
+from .models import Goods
 
 class GoodsSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=100)
     click_num = serializers.IntegerField(default=0)
     goods_front_image=serializers.ImageField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Goods.objects.create(**validated_data)
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,7 @@
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Goods
 
 # Create your views here.
@@ -13,3 +14,10 @@ class GoodsListView(APIView):
         goods = Goods.objects.all()[:10]
         goods_serializer = GoodsSerializer(goods, many=True)
         return Response(goods_serializer.data)
+
+    def post(self,request):
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
